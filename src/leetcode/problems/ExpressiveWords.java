9d4f14898141a5546bf9abbfba51c72bd757244a
Madhu-Sanduri <madhusanduri343@gmail.com>
2025-09-02 16:40:25 +0530

    Septemeber 1st Challenge

--- src/leetcode/problems/ExpressiveWords.java
+++ src/leetcode/problems/ExpressiveWords.java
@@ -1,4 +1,14 @@
 package leetcode.problems;
 
 public class ExpressiveWords {
+
+    public static int solution(String s,String []words){
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(solution("heeellooo",new String[]{"hello", "hi", "helo"}));
+        System.out.println(solution("zzzzzyyyyy",new String[]{"zzyy","zy","zyy"}));
+    }
 }
